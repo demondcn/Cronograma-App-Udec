212d5236ee72ad41c131d96ac9c8c8752e8a2175
demondcn <guerrero70407@gmail.com>
2025-08-23 20:41:23 -0500

    solucionando produccion 7

--- cronograma-dashboard/components/Traedores/actions/aulasNombre.ts
+++ cronograma-dashboard/components/Traedores/actions/aulasNombre.ts
@@ -1,6 +1,7 @@
 'use server'
 
-import prisma from './../../../lib/db'
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient()
 
 export async function obtenerAulas() {
   try {
